94200bbbbf03bf46e661a1175f6cb0ef9b314107
Mariusz Chałaj <mariusz.ch88@wp.pl>
2023-11-27 10:25:54 +0100

    zadanie-zakończone

--- zadanie-48108/img/after/js/script.js
+++ zadanie-48108/img/after/js/script.js
@@ -1,79 +1,74 @@
-console.log('Warsztat - Infinite scroll');
+console.log(`Warsztat - Infinite scroll`);
 
 let endOfThePage = 0;
 
-let preLoading = false;
-
+let preloading = false;
 
 
 const showPreloader = () => {
 
     let preloader = document.getElementById('preloader');
-    console.log('showPreloader()');
+    console.log(`showPreloader()`);
     preloader.style.display = 'block';
-    preLoading = true;
+    preloading = true;
+
 }
+
 
 const hidePreloader = () => {
 
     let preloader = document.getElementById('preloader');
-    console.log('hidePreloader()');
+    console.log(`hidePreloader()`);
     preloader.style.display = 'none';
-    preLoading = false;
+    preloading = false;
+    
 }
 
-const getData = () => {
 
-    if (!preLoading) {
+const getData = () => {
+    
+    if (!preloading) {
 
         showPreloader();
-
-
 
         fetch('https://akademia108.pl/api/ajax/get-users.php')
             .then(res => res.json())
             .then(data => {
-
+    
                 let body = document.body;
                 let hr = document.createElement('hr');
                 body.appendChild(hr);
-
+    
                 for (let user of data) {
                     let pId = document.createElement('p');
                     let pName = document.createElement('p');
                     let pWebsite = document.createElement('p');
-
+    
                     pId.innerText = `User ID: ${user.id}`;
                     pName.innerText = `User Name: ${user.name}`;
-                    pWebsite.innerHTML = `User URL: ${user.website}<br />----------`;
-
-
+                    pWebsite.innerHTML = `User URL: ${user.website}<br />--------`;
+    
                     body.appendChild(pId);
                     body.appendChild(pName);
                     body.appendChild(pWebsite);
-
-
                 }
-
-
-
+                
                 hidePreloader();
-
+    
                 console.log(data);
             })
             .catch(error => {
                 console.log(error);
-            })
+            });
     }
-
+    
 }
 
-
 const scrollToEndOfPage = () => {
-
+    
     let d = document.documentElement;
 
-    // height of an element's content, includind content not visible on the screen
+    // height of an element's content, including content not visible on the screen
     let scrollHeight = d.scrollHeight;
 
     // number of pixels that an element's content is scrolled vertically
@@ -90,18 +85,17 @@ const scrollToEndOfPage = () => {
     console.log(`sumScrollTopClientHeight: ${sumScrollTopClientHeight}`);
     console.log(`scrollTop: ${scrollTop}`);
     console.log(`clientHeight: ${clientHeight}`);
-    console.log(`==================`);
+    console.log(`=====================`);
+
 
     if (sumScrollTopClientHeight >= scrollHeight) {
 
         endOfThePage += 1;
-        console.log('Scrolled to the end of page: ${"endOfThePage"}');
 
-
+        console.log(`Scrolled to the end of page: ${endOfThePage}`);
 
         getData();
     }
-
 
 }
 
